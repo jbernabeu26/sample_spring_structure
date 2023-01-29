ea547f6afcbea546c7f0a31fe132d0d39db05bc8
kumarbhupi <kumar.bhupi13@gmail.com>
2023-01-29 16:40:15 +0100

    added new method to find all users and introduced mockmvc to test isolated code parts.

--- src/main/java/com/waigo/backend_api/Controllers/UserController.java
+++ src/main/java/com/waigo/backend_api/Controllers/UserController.java
@@ -1,11 +1,27 @@
 package com.waigo.backend_api.Controllers;
 
 
+import com.waigo.backend_api.Model.Entities.User;
+import com.waigo.backend_api.Services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService injectedBean) {
+        this.userService = injectedBean;
+    }
+
+    @RequestMapping(path = "/all", method = RequestMethod.GET)
+    public @ResponseBody List<User> findAllUsers() {
+        return userService.findAll();
+    }
+
 
 }
--- src/main/java/com/waigo/backend_api/Services/UserService.java
+++ src/main/java/com/waigo/backend_api/Services/UserService.java
@@ -1,6 +1,11 @@
 package com.waigo.backend_api.Services;
 
+import com.waigo.backend_api.Model.Entities.User;
+
+import java.util.List;
+
 public interface UserService {
     // AQUI VAN DECLARADOS TODOS LOS METODOS QUE UTILIZAREMOS PARA LOS USUARIOS
+    List<User> findAll();
 
 }
