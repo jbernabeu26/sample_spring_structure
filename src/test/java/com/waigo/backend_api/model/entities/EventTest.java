32bdb59ba7e3140b13472ac3dc0bfb52c7e2dcee
Ivan <ivanjimenezvalencia4@gmail.com>
2023-02-15 18:58:35 +0100

    Test testCreatingEventWithoutNameField done

--- src/test/java/com/waigo/backend_api/model/entities/EventTest.java
+++ src/test/java/com/waigo/backend_api/model/entities/EventTest.java
@@ -5,18 +5,14 @@ import com.waigo.backend_api.model.repositories.EventRepository;
 import com.waigo.backend_api.model.repositories.UserRepository;
 import com.waigo.backend_api.utils.SetUp;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
@@ -25,16 +21,19 @@ public class EventTest {
 
     @Autowired
     private EventRepository eventRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     private final SetUp data = new SetUp();
-    @BeforeEach
-    public void setUp() {eventRepository.deleteAll();}
+    @Before
+    public void setUp() {
+        userRepository.saveAndFlush(data.getValidCustomUser());
+    }
 
     @Test
     public void testCreatingEventWithoutNameField(){
         final Event event = new Event(null, data.getValidDescription(), data.getValidStartDate(), data.getValidEndDate(), data.getValidPrivacy(), data.getValidMaxParticipants(),
                 data.getValidCategorySet(), data.getValidCustomUser(), data.getValidGeolocation());
-        System.out.println(eventRepository.saveAndFlush(event));
         Assertions.assertThatThrownBy(() -> eventRepository.saveAndFlush(event)).hasMessageContaining("event.null_name");
     }
 
--- src/test/java/com/waigo/backend_api/utils/SetUp.java
+++ src/test/java/com/waigo/backend_api/utils/SetUp.java
@@ -24,18 +24,6 @@ public class SetUp {
     @Getter private final String category_name_2_Chars = new String(new char[2]).replace('\0','a');
     @Getter private final String category_name_3_Chars = new String(new char[3]).replace('\0','a');
 
-    private final Category getRandomCategory(int length){
-        {
-            Category new_category;
-            Random random = new Random();
-            char[] name = new char[length];
-            for (int i = 0; i < length; i++) {
-                char c = (char) (random.nextInt(26) + 'a');
-                name[i] = c;
-            }
-            new_category = new Category(String.valueOf(name));
-            return new_category;
-    }
 
     /** SET UP FOR CUSTOM USER TESTS**/
     @Getter private final String validDescription = new String(new char[250]).replace("\0", "a");
@@ -43,7 +31,7 @@ public class SetUp {
     @Getter private final String validFirstName = "John";
     @Getter private final String validLastName = "Doe";
     @Getter private final String validEmail = "john.doe@mail.com";
-    @Getter private final CustomUser validCustomUser = new CustomUser(validDescription, validLastName, validEmail, validPassword, validDescription);
+    @Getter private final CustomUser validCustomUser = new CustomUser(validFirstName, validLastName, validEmail, validPassword, validDescription);
 
     /** SET UP FOR EVENT TESTS **/
     @Getter final LocalDateTime validStartDate = LocalDateTime.of(2023, 12, 12, 12, 0);
@@ -53,6 +41,17 @@ public class SetUp {
     @Getter final Event.PrivacyStatus validPrivacy = Event.PrivacyStatus.MIXED;
     @Getter final Set<Category> validCategorySet =  new HashSet<>(Arrays.asList(getRandomCategory(10), getRandomCategory(10)));
 
+    private final Category getRandomCategory(int length){
 
+            Category new_category;
+            Random random = new Random();
+            char[] name = new char[length];
+            for (int i = 0; i < length; i++) {
+                char c = (char) (random.nextInt(26) + 'a');
+                name[i] = c;
+            }
+            new_category = new Category(String.valueOf(name));
+            return new_category;
+        }
     
 }
