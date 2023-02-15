3b7ebf83d6cbce94aafb1634911d68cc714ef296
kumarbhupi <kumar.bhupi13@gmail.com>
2023-02-15 20:04:02 +0100

    initial UserServiceTest

--- src/test/java/com/waigo/backend_api/services/CustomUserServiceTests.java
+++ src/test/java/com/waigo/backend_api/services/CustomUserServiceTests.java
@@ -1,40 +1,28 @@
 package com.waigo.backend_api.services;
 
-import com.waigo.backend_api.model.entities.CustomUser;
+import com.waigo.backend_api.utils.MockDataGenerator;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserService.class)
 public class CustomUserServiceTests {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @MockBean
     private UserService userService;
 
-    @Test
-    public void shouldReturnDefaultMessageWhenRequestForGetAllUsers() throws Exception {
-        List<CustomUser> customUsers = new ArrayList<>();
-        //Testing service method
-        when(userService.findAll()).thenReturn(customUsers);
+    private MockDataGenerator data;
 
-        //Consuming the endpoint
-        this.mockMvc.perform(get("/users/all")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("")));
+
+    //Testing create user operations
+
+    @Test
+    public void createUserWithWrongBlankFirstName(){
+
+
     }
+
 
 }
