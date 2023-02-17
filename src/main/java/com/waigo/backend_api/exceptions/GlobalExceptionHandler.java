592436a2256b85e11b866921b4949e35106efb70
kumarbhupi <kumar.bhupi13@gmail.com>
2023-02-17 11:02:08 +0100

    cleanup

--- src/main/java/com/waigo/backend_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/waigo/backend_api/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.waigo.backend_api.exceptions;
 
 import com.waigo.backend_api.utils.TranslatorExceptions;
 import lombok.RequiredArgsConstructor;
+import com.waigo.backend_api.utils.TranslatorExceptions;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
@@ -13,25 +15,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
 import java.util.HashMap;
 import java.util.List;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @Autowired
-    private TranslatorExceptions translatorExceptions;
+    private final TranslatorExceptions translatorExceptions;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        System.out.println("handleMethodArgumentNotValid " + ex.getMessage());
         Map<String, Object> body = new HashMap<>();
 
         List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
-        Map<String, Map<String, Set<String>>> errors2 = translatorExceptions.translateExceptionMessages(errors);
-        body.put("errors", errors2);
+        Map<String, Map<String, Set<String>>> mappedErrors = translatorExceptions.translateExceptionMessages(errors);
+        body.put("errors", mappedErrors);
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/waigo/backend_api/utils/TranslatorExceptions.java
+++ src/main/java/com/waigo/backend_api/utils/TranslatorExceptions.java
@@ -1,6 +1,6 @@
 package com.waigo.backend_api.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,28 +10,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
+@Qualifier("messageSource")
 public class TranslatorExceptions {
 
-    @Autowired
-    @Qualifier("messageSource")
-    MessageSource messageSource;
-    public Map<String, Map<String, Set<String>>> translateExceptionMessages(List<String> codes){
 
-        Map<String, Map<String, Set<String>>> errorsMap = codes.stream()
-                .map(errors -> errors.split("\\.",3))
+    private final MessageSource messageSource;
+
+    public Map<String, Map<String, Set<String>>> translateExceptionMessages(List<String> codes) {
+        return codes.stream()
+                .map(errors -> errors.split("\\.", 3))
                 .collect(Collectors.groupingBy(
                         entity -> entity[0],
                         Collectors.groupingBy(
                                 field -> field[1],
-                                Collectors.mapping(error -> messageSource.getMessage(error[0]+"."+error[1]+"."+error[2],null, LocaleContextHolder.getLocale()), Collectors.toSet())
+                                Collectors.mapping(error -> messageSource.getMessage(error[0] + "." + error[1] + "." + error[2], null, LocaleContextHolder.getLocale()), Collectors.toSet())
                         )
                 ));
 
 
-        return errorsMap;
     }
 
-    public String translateExceptionMessage(String code){
-        return messageSource.getMessage(code,null, LocaleContextHolder.getLocale());
+    public String translateExceptionMessage(String code) {
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
     }
 }
